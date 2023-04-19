64f734660c44855da3139f4c7d383f196d4ed433
Atiye.beigirad@gmail.com <atiye.beigirad@gmail.com>
2023-04-19 17:47:40 +0330

    set to show the value of searchbox

--- src/component/searchbox.js
+++ src/component/searchbox.js
@@ -1,17 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 import "./searchbox.css";
 
 export default function Search() {
+  const [city, setcity] = useState(" ");
+  const [message, setmessage] = useState("");
+  function showvalue(event) {
+    event.preventDefault();
+    setmessage(`searching for ${city}`);
+  }
+  function changecity(event) {
+    setcity(event.target.value);
+  }
   return (
     <div className="App">
-      <div className="box">
+      <form className="box" onSubmit={showvalue}>
         <button>
           <FontAwesomeIcon icon={faSearch} size="xl" />
         </button>
-        <input type="search" placeholder="type a city" />
-      </div>
+        <input type="search" placeholder="type a city" onChange={changecity} />
+      </form>
+      <h2>{message}</h2>
     </div>
   );
 }
