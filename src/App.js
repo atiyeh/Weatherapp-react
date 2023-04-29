c8ac278af7e5f6d335a449ddf666cb27a41a414d
Atiye.beigirad@gmail.com <atiye.beigirad@gmail.com>
2023-04-30 00:54:59 +0330

    compress css styles in 1 css file

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import Search from "./component/searchbox";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import "./index.css";
+import Forcast from "./pages/forcast";
 
 function App() {
     return (
-        <div className="Search">
-            <div className="container">
-                <Search defaultcity="New york" />
-            </div>
-        </div>
+        <BrowserRouter>
+            <Routes>
+                <Route index element={<Search defaultcity="New york" />} />
+                <Route path="forcast" element={<Forcast />} />
+            </Routes>
+        </BrowserRouter>
     );
 }
-
 export default App;
---
+++ src/pages/forcast.js
@@ -0,0 +1,8 @@
+import React from "react";
+export default function Forcast() {
+    return (
+        <div>
+            <h1>helllo world</h1>
+        </div>
+    );
+}
